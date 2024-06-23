0a2dca2451ae7066d00cce171117e5bdee3f27bd
RithanyaG <111141560+RithanyaG@users.noreply.github.com>
2024-06-23 11:52:48 +0530

    Create Nth Fibonacci.java

---
+++ Nth Fibonacci.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static int fibo(int n){
+        int a=0;
+        int b=1;
+        if(n==1)
+            return a;
+        for(int i=3;i<=n;i++){
+            int c=a+b;
+            a=b;
+            b=c;
+        }
+        return b;
+    }
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		System.out.println(fibo(n));
+		}
+}
